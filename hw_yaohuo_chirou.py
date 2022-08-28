8b3f732784f3451d0240c13c4dc89b481c8f5d63
院长 <32212075+yuanter@users.noreply.github.com>
2022-08-28 10:37:29 +0800

    Update hw_yaohuo_chirou.py

--- hw_yaohuo_chirou.py
+++ hw_yaohuo_chirou.py
@@ -18,7 +18,7 @@ PUSH_PLUS_TOKEN = os.getenv("PUSH_PLUS_TOKEN", PUSH_PLUS_TOKEN)
 if PUSH_PLUS_TOKEN is None:
     PUSH_PLUS_TOKEN = ""
 
-Rou_IDs = open(r'IDs.txt','w+', encoding='utf-8')
+
 
 def Get_ID(cookie):
     HEADERS = {
@@ -61,6 +61,7 @@ def main(cookie,sid):
     JingYan = 0
     dic=[]
     reqtext = ["chi", "吃", "c", "恰", "奥利给", "吃了", "吃吃吃","吃吃"]
+    Rou_IDs = open(r'IDs.txt','w+', encoding='utf-8')
     for item in Rou_IDs:
         item = item.replace('\n','')
         dic.append(item)
